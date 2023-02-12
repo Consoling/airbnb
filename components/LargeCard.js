65321724e304eb642105233b76b91ff365f0e9b5
Consoling <shahidarizfannymagnet@gmail.com>
2023-02-12 21:34:47 +0530

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   README.md 	new file:   components/Banner.js 	new file:   components/Footer.js 	new file:   components/Header.js 	new file:   components/LargeCard.js 	new file:   components/MediumCard.js 	new file:   components/SmallCard.js 	new file:   jsconfig.json 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   pages/_app.js 	new file:   pages/_document.js 	new file:   pages/api/hello.js 	new file:   pages/index.js 	new file:   postcss.config.js 	new file:   public/favicon.ico 	new file:   public/next.svg 	new file:   public/thirteen.svg 	new file:   public/vercel.svg 	new file:   styles/Home.module.css 	new file:   styles/globals.css 	new file:   tailwind.config.js

---
+++ components/LargeCard.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Image from 'next/legacy/image'
+
+function LargeCard({ img, title, description, buttonText}) {
+  return (
+    <section className='relative py-16 cursor-pointer'>
+        <div className="relative h-96 min-w-[300px]">
+            <Image 
+            src={img}
+            layout = "fill"
+            objectFit = "cover"
+            className='rounded-2xl'
+            />
+
+
+        </div>
+
+        <div className='absolute top-32 left-12'>
+            <h3 className='text-4xl mb-3 w-64'>
+                {title}
+                <p>
+                    {description}
+                    </p>
+                    <button className='text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5'>
+                        {buttonText}
+                    </button>
+            </h3>
+
+        </div>
+      
+    </section>
+  )
+}
+
+export default LargeCard
